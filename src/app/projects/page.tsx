743eb16bf4cd671719480e109e091229c596a49f
Azka <falcxxdev@pm.me>
2025-06-09 09:16:29 +0000

    feat(route): add projects route & loading handler

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,72 @@
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import { createClient } from '@/utils/supabase/client';
+import { Container } from '@/components/Container';
+import { Icon } from '@iconify/react';
+import Image from 'next/image';
+
+type ProjectsProps = {
+    data: {
+        id: number;
+        created_at: string;
+        name: string;
+        description: string;
+        image: string;
+        url: string;
+        source: string;
+    }[];
+};
+
+export default async function Projects() {
+    const supabase = createClient();
+    const projects: ProjectsProps = (await supabase.from('Projects').select('*')) as any;
+
+    return (
+        <Container>
+            <div className="md:flex items-center px-5 py-10 md:px-10">
+                <div className="flex flex-col gap-4">
+                    <h1 className="text-4xl font-black">Projects</h1>
+                    <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+                        {projects.data.map(data => {
+                            return (
+                                <Card key={data.id} className="py-0">
+                                    <CardHeader className="border-b-2 border-black px-0 gap-0 overflow-hidden">
+                                        <div className="overflow-hidden">
+                                            <a href={data.url || data.source} target="_blank">
+                                                <Image
+                                                    src={data.image}
+                                                    alt={data.name}
+                                                    width={700}
+                                                    height={500}
+                                                    className="w-[100%]"
+                                                />
+                                            </a>
+                                        </div>
+                                    </CardHeader>
+                                    <CardContent className="pb-6">
+                                        <div className="flex pb-2">
+                                            <CardTitle>{data.name}</CardTitle>
+                                            <div className="flex flex-1 items-center align-middle space-x-2 justify-end">
+                                                {data.source ? (
+                                                    <a href={data.source} target="_blank">
+                                                        <Icon icon="mdi:github" className="size-5" />
+                                                    </a>
+                                                ) : null}
+                                                {data.url ? (
+                                                    <a href={data.url} target="_blank">
+                                                        <Icon icon="majesticons:open" className="size-5" />
+                                                    </a>
+                                                ) : null}
+                                            </div>
+                                        </div>
+                                        <CardDescription>{data.description}</CardDescription>
+                                    </CardContent>
+                                </Card>
+                            );
+                        })}
+                    </div>
+                </div>
+            </div>
+            {/* <code className="block bg-main/30 p-4 rounded-2xl">{JSON.stringify(projects)}</code> */}
+        </Container>
+    );
+}
