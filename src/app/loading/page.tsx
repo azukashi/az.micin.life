6deef44164a7c72d96b4a3b2215b8862ca6e9f93
Azka <falcxxdev@pm.me>
2025-06-09 09:16:05 +0000

    chore(loading): update loading test page

--- src/app/loading/page.tsx
+++ src/app/loading/page.tsx
@@ -4,17 +4,13 @@ import { Skeleton } from '@/components/ui/skeleton';
 export default function Loading() {
     return (
         <Container>
-            <div className="-mt-17 px-10 space-x-2 flex items-center min-h-screen">
-                <div className="space-y-2">
+            <div className="md:-mt-17 md:flex space-x-2 items-center min-h-screen px-5 py-10 md:px-10">
+                <div className="flex flex-col gap-4 min-w-full">
                     <Skeleton className="h-12 md:w-[250px] w-[250px]" />
-                    <Skeleton className="h-16 md:w-[200px] w-[200px]" />
-                    <Skeleton className="h-8 mt-8 md:w-[400px] w-[250px]" />
-                    <Skeleton className="h-8 md:w-[400px] w-[250px]" />
-                    <Skeleton className="h-8 mt-8 md:w-[300px] w-[250px]" />
-                    <Skeleton className="h-8 md:w-[300px] w-[250px]" />
-                    <div className="flex mt-8 space-x-2">
-                        <Skeleton className="h-10 md:w-[100px] w-[100px]" />
-                        <Skeleton className="h-10 md:w-[100px] w-[100px]" />
+                    <div className="grid grid-cols-1 md:grid-cols-3 gap-4 min-h-screen md:min-h-0">
+                        <Skeleton className="h-[100%] md:h-72 md:w-full w-full" />
+                        <Skeleton className="h-[100%] md:h-72 md:w-full w-full" />
+                        <Skeleton className="h-[100%] md:h-72 md:w-full w-full" />
                     </div>
                 </div>
             </div>
