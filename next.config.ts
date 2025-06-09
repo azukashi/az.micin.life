7ec0a828dcc19eca5f283e5440cd8d7efc2169f6
Azka <falcxxdev@pm.me>
2025-06-09 09:16:52 +0000

    chore(next): add azukashiic.sirv.com to remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
     images: {
-        remotePatterns: [new URL('https://avatars.githubusercontent.com/u/*')],
+        remotePatterns: [
+            new URL('https://avatars.githubusercontent.com/u/*'),
+            new URL('https://azukashiic.sirv.com/assets/portfolio/projects/*.png?format=original&q=100'),
+        ],
     },
 };
 
