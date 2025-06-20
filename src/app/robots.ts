53c3d3b445cf1d5d25edcbe019db7c656312f045
Azka <falcxxdev@pm.me>
2025-06-20 06:46:05 +0000

    chore(seo): add sitemap object to robots file

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,5 +6,6 @@ export default function robots(): MetadataRoute.Robots {
             userAgent: '*',
             allow: '/',
         },
+        sitemap: 'https://az.micin.life/sitemap.xml',
     };
 }
